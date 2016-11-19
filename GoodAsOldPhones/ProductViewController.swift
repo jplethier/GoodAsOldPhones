ba131a37fcfd502e91c37278297fd02f3081588e
João Paulo Lethier <jplethier@gmail.com>
2016-11-19 16:09:58 -0200

    setting image view on product view controller

--- GoodAsOldPhones/ProductViewController.swift
+++ GoodAsOldPhones/ProductViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ProductViewController: UIViewController {
 
+    @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var productNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = #imageLiteral(resourceName: "phone-fullscreen3")
     }
 }
