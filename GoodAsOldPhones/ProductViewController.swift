574d8999dbf9297b0839140550505c0c699472b9
João Paulo Lethier <jplethier@gmail.com>
2016-11-19 15:53:00 -0200

    first storyboard

---
+++ GoodAsOldPhones/ProductViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Joao Paulo Lethier on 19/11/16.
+//  Copyright © 2016 Joao Paulo Lethier. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
