996ac61cf6ae3d1a2e37f42f14dcd8b597bbb698
João Paulo Lethier <jplethier@gmail.com>
2016-11-19 22:52:51 -0200

    table view structure

---
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProductsTableViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Joao Paulo Lethier on 19/11/16.
+//  Copyright © 2016 Joao Paulo Lethier. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        
+        return cell
+    }
+}
